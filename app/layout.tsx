e1f122365448cf1c46181e2301189ae3595de3ce
ValeriiaM <moskalenkovvy@gmail.com>
2026-03-13 23:20:23 +0200

    add global and common styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope, Inter } from "next/font/google";
 import "./globals.css";
+import "./common.css";
 
-const geistSans = Geist({
+const manropeSans = Manrope({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const interSans = Inter({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -24,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${manropeSans.variable} ${interSans.variable}`}>
         {children}
       </body>
     </html>
